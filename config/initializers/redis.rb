6981e6787faa79a3dbd8139b732ec24017f52509
haider-wazir <hadeem313@gmail.com>
2025-01-31 05:43:13 +0300

    Simplify Redis configuration

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,16 +1,3 @@
 require 'redis'
-require 'openssl'
 
-redis_url = ENV.fetch('REDIS_URL')
-uri = URI.parse(redis_url)
-
-$redis = Redis.new(
-  url: redis_url,
-  ssl: true,
-  ssl_params: {
-    verify_mode: OpenSSL::SSL::VERIFY_NONE,
-    verify_hostname: false,
-    ssl_version: :TLSv1_2,
-    cert_store: OpenSSL::X509::Store.new
-  }
-)
+$redis = Redis.new(url: ENV.fetch('REDIS_URL'))
