65c64dee0ae3de7a474d68a933b92c5280bd911c
haider-wazir <hadeem313@gmail.com>
2025-01-31 05:59:26 +0300

    Use TLS 1.2 for Redis and Action Cable

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -5,9 +5,9 @@ redis_url = ENV.fetch('REDIS_URL')
 
 $redis = Redis.new(
   url: redis_url,
-  driver: :ruby,
   ssl: true,
   ssl_params: {
-    verify_mode: OpenSSL::SSL::VERIFY_NONE
+    verify_mode: OpenSSL::SSL::VERIFY_NONE,
+    ssl_version: :TLSv1_2
   }
 )
