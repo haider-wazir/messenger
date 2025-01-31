3f110b9fd07dcaaad12e0eed547dbf32c4efa45a
haider-wazir <hadeem313@gmail.com>
2025-01-31 06:02:51 +0300

    Simplify Redis and Action Cable configuration according to Heroku docs

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,13 +1,6 @@
 require 'redis'
-require 'openssl'
-
-redis_url = ENV.fetch('REDIS_URL')
 
 $redis = Redis.new(
-  url: redis_url,
-  ssl: true,
-  ssl_params: {
-    verify_mode: OpenSSL::SSL::VERIFY_NONE,
-    ssl_version: :TLSv1_2
-  }
+  url: ENV.fetch('REDIS_URL'),
+  ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE }
 )
