fe7f483b6ac43438e8cc47a55c3271895e70005c
haider-wazir <hadeem313@gmail.com>
2025-01-31 05:32:31 +0300

    Configure Redis with conditional SSL based on URL scheme

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,3 +1,17 @@
 require 'redis'
+require 'openssl'
 
-$redis = Redis.new(url: ENV.fetch('REDIS_URL'))
+redis_url = ENV.fetch('REDIS_URL')
+uri = URI.parse(redis_url)
+
+$redis = if uri.scheme == 'rediss'
+  Redis.new(
+    url: redis_url,
+    ssl: true,
+    ssl_params: {
+      verify_mode: OpenSSL::SSL::VERIFY_NONE
+    }
+  )
+else
+  Redis.new(url: redis_url)
+end
