f3fb464935344764b9ebf493925f4ecd1414d0f8
haider-wazir <hadeem313@gmail.com>
2025-01-31 05:25:30 +0300

    Configure Redis SSL with hostname verification disabled

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -2,7 +2,9 @@ require 'redis'
 
 $redis = Redis.new(
   url: ENV.fetch('REDIS_URL'),
+  ssl: true,
   ssl_params: {
-    verify_mode: OpenSSL::SSL::VERIFY_NONE
+    verify_mode: OpenSSL::SSL::VERIFY_PEER,
+    verify_hostname: false
   }
 )
