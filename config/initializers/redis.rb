c4d48090174559ac489e42435bccc60d29c7e564
haider-wazir <hadeem313@gmail.com>
2025-01-31 05:47:04 +0300

    Use Heroku's CA certificate path for Redis SSL

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,10 +1,14 @@
 require 'redis'
-require 'uri'
+require 'openssl'
 
 redis_url = ENV.fetch('REDIS_URL')
 uri = URI.parse(redis_url)
 
-# Convert rediss:// to redis:// to bypass SSL
-redis_url = redis_url.gsub('rediss://', 'redis://')
-
-$redis = Redis.new(url: redis_url)
+$redis = Redis.new(
+  url: redis_url,
+  ssl: true,
+  ssl_params: {
+    verify_mode: OpenSSL::SSL::VERIFY_PEER,
+    ca_file: '/etc/ssl/certs/ca-certificates.crt'
+  }
+)
