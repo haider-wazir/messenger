5dbe2bf3254ba3ad55e0c9c2be7d2afd67f8136c
haider-wazir <hadeem313@gmail.com>
2025-01-31 05:37:48 +0300

    Use Redis URL with TLS 1.2 and hostname verification disabled

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,15 +1,15 @@
 require 'redis'
 require 'openssl'
 
-redis_options = { url: ENV.fetch('REDIS_URL') }
+redis_url = ENV.fetch('REDIS_URL')
+uri = URI.parse(redis_url)
 
-if Rails.env.production?
-  redis_options.merge!(
-    ssl: true,
-    ssl_params: {
-      verify_mode: OpenSSL::SSL::VERIFY_NONE
-    }
-  )
-end
-
-$redis = Redis.new(redis_options)
+$redis = Redis.new(
+  url: redis_url,
+  ssl: true,
+  ssl_params: {
+    verify_mode: OpenSSL::SSL::VERIFY_NONE,
+    verify_hostname: false,
+    ssl_version: :TLSv1_2
+  }
+)
