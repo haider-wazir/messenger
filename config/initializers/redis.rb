48e5fabd316319133efab1df6e3ffebd587ab458
haider-wazir <hadeem313@gmail.com>
2025-01-31 06:24:57 +0300

    Add explicit SSL flag and disable hostname verification

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,6 +1,14 @@
 require 'redis'
+require 'uri'
+
+redis_url = ENV.fetch('REDIS_URL')
+uri = URI.parse(redis_url)
 
 $redis = Redis.new(
-  url: ENV.fetch('REDIS_URL'),
-  ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE }
+  url: redis_url,
+  ssl: uri.scheme == 'rediss',
+  ssl_params: { 
+    verify_mode: OpenSSL::SSL::VERIFY_NONE,
+    verify_hostname: false
+  }
 )
