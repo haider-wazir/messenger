ad7c6402e831253751ae17a9466a3344e9fbf47d
haider-wazir <hadeem313@gmail.com>
2025-01-31 05:54:44 +0300

    Use Ruby driver for Redis with SSL verification disabled

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -2,16 +2,12 @@ require 'redis'
 require 'openssl'
 
 redis_url = ENV.fetch('REDIS_URL')
-uri = URI.parse(redis_url)
-
-cert_store = OpenSSL::X509::Store.new
-cert_store.set_default_paths
 
 $redis = Redis.new(
   url: redis_url,
+  driver: :ruby,
   ssl: true,
   ssl_params: {
-    verify_mode: OpenSSL::SSL::VERIFY_PEER,
-    cert_store: cert_store
+    verify_mode: OpenSSL::SSL::VERIFY_NONE
   }
 )
