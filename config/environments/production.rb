27d2a46037191f549cfe95698f07bbef7b87d591
haider-wazir <hadeem313@gmail.com>
2025-01-31 04:40:19 +0300

    Fix production asset configuration for Vite

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -26,20 +26,8 @@ Rails.application.configure do
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
-  # Enable serving of files from the /public folder
-  config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
-
-  # Enable serving of files from the /app/javascript folder
-  config.serve_static_assets = ENV['RAILS_SERVE_STATIC_FILES'].present?
-
-  # Enable serving of static files from the /public folder
-  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
-
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
-
-  # Enable serving of files from the /app/javascript folder
-  config.serve_static_assets = true
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.asset_host = "http://assets.example.com"
