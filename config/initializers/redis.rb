6d19684be69e57ff1d77a4e2f6880adeda4061ad
haider-wazir <hadeem313@gmail.com>
2025-01-31 05:26:58 +0300

    Use custom CA certificate path for Redis SSL

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,9 +1,11 @@
 require 'redis'
+require 'openssl'
 
 $redis = Redis.new(
   url: ENV.fetch('REDIS_URL'),
   ssl: true,
   ssl_params: {
+    ca_file: ENV.fetch('SSL_CERT_FILE', '/etc/ssl/certs/ca-certificates.crt'),
     verify_mode: OpenSSL::SSL::VERIFY_PEER,
     verify_hostname: false
   }
