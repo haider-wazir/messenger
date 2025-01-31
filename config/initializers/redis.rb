1e265d6c80cdb23a2dbdcdf67bfe733587ec7e2c
haider-wazir <hadeem313@gmail.com>
2025-01-31 05:23:45 +0300

    Use Heroku Redis recommended configuration

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,16 +1,8 @@
 require 'redis'
-require 'open-uri'
-require 'tempfile'
-
-ca_cert_url = 'https://raw.githubusercontent.com/heroku/heroku-buildpack-ruby/main/certs/heroku-redis.crt'
-ca_cert_file = Tempfile.new('heroku-redis.crt')
-ca_cert_file.write(URI.open(ca_cert_url).read)
-ca_cert_file.close
 
 $redis = Redis.new(
   url: ENV.fetch('REDIS_URL'),
-  ssl: true,
   ssl_params: {
-    ca_file: ca_cert_file.path
+    verify_mode: OpenSSL::SSL::VERIFY_NONE
   }
 )
